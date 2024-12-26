8817bfd93d2eef9d65252350939ceb145802873f
pk1427 <prasadkapure553@gmail.com>
2024-12-27 00:27:27 +0530

    verify-identity

---
+++ app/verify-identity/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navbar/navbar";
+import Section from "./section";
+
+const VerifyIdentiy = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <Section />
+
+    </div>
+  );
+};
+
+export default VerifyIdentiy;
